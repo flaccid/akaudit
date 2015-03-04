553ba1d37ed5d90be138b764c464fc8531c09d37
Chris Fordham <chris@xhost.com.au>
2015-03-04 18:11:58 +1100

    Implement dry with remove_public_key().

--- akaudit/keytools.py
+++ akaudit/keytools.py
@@ -1,10 +1,10 @@
-def remove_public_key(ak=None, key=None, dry=True):
-	print(ak, key)
+def remove_public_key(ak=None, key=None, dry=False):
 	f = open(ak, 'r')
 	lines = f.readlines()
 	f.close()
-	f = open(ak, 'w')
-	for line in lines:
-		if key not in line:
-			f.write(line)
-	f.close()
+	if not dry:
+		f = open(ak, 'w')
+		for line in lines:
+			if key not in line:
+				f.write(line)
+		f.close()
