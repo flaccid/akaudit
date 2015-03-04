3412fc49864ba83f0dfabcaaf532594613f00991
Chris Fordham <chris@xhost.com.au>
2015-03-04 17:31:09 +1100

    Add userinput module.

---
+++ akaudit/userinput.py
@@ -0,0 +1,12 @@
+def yesno(prompt='? '):
+	# raw_input returns the empty string for "enter"
+	yes = set(['yes','y', 'ye', ''])
+	no = set(['no','n'])
+
+	choice = input(prompt).lower()
+	if choice in yes:
+		return True
+	elif choice in no:
+		return False
+	else:
+		sys.stdout.write("Please respond with 'yes' or 'no'")
