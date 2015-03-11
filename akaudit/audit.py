529125a4a69c563b18c7f990bd267d3e16742476
Chris Fordham <chris.fordham@industrieit.com>
2015-03-11 23:08:16 +1100

    Fe-factor for audit to use authorized_keys and minor improvements.

--- akaudit/audit.py
+++ akaudit/audit.py
@@ -19,11 +19,17 @@ import sys
 import platform
 import pwd, grp
 import logging
+import base64
+import akaudit.authorized_keys
 import akaudit.keytools
+from akaudit.authorized_keys import PublicKey
 from paramiko import SSHClient, SSHConfig
-from colorama import Fore, Back, Style
+from colorama import init, Fore, Back, Style
 from akaudit.userinput import yesno
 from akaudit.keytools import remove_public_key
+
+# https://pypi.python.org/pypi/colorama
+init(autoreset=True)
 
 class Auditer():
     def run_audit(self, args):
@@ -52,33 +58,59 @@ class Auditer():
             else:
                 authorized_keys_file = '.ssh/authorized_keys'
         else:
-            raise Exception('Cannot find a local sshd_config in commonly installed paths.')
+            raise Exception('Cannot find a local sshd_config in commonly installed paths')
 
-        logging.debug(str('sshd accepts keys in ' + authorized_keys_file + ' for users.'))
+        logging.debug(str('sshd accepts keys in ' + authorized_keys_file + ' for users'))
 
         for p in pwd.getpwall():
+            # mega debug
+            # logging.debug(str('parsing user, ' + p[0]))
             if p[6] not in invalid_shells:
                 home_dir = p[5]
                 ak_path = os.path.join(home_dir, authorized_keys_file)
-                if os.path.isfile(ak_path):
-                    logging.info(str(p[0] + ' has authorized public keys!'))
+                if os.path.isfile(ak_path) and os.path.getsize(ak_path) > 0:
+                    logging.info(str('interrogating user, ' + p[0]))
+                    if not logging.getLogger().isEnabledFor(logging.DEBUG):
+                        print()
                     logging.debug(str('user: ' + p[0] + ', shell: ' + p[6] + ', home: ' +  p[5]))
-                    lines = [line.strip() for line in open(ak_path) if line.strip()]
-                    for line in lines:
-                        logging.debug(Fore.YELLOW + str(line.split()))
-                        public_key = line.split()[1]
-                        if len(line.split()) > 2:
-                            label = line.split()[2]
-                        else:
-                            label = '<no label>'
-                        print(ak_path, ':', label, ':', public_key[0:12] + '...' + public_key[-19:])
-                        if args.interactive:
-                            if yesno('==> Remove key? '):
-                                remove_public_key(ak_path, public_key)
-                                logging.info(Fore.GREEN + "Key '" + label + "' removed.")
-                            else:
-                                logging.debug(Fore.GREEN + "Key removal of '" + label + "' skipped.")
-                            sys.stdout.write(Fore.RESET + Back.RESET + Style.RESET_ALL)
 
-                        sys.stdout.write(Fore.RESET + Back.RESET + Style.RESET_ALL)
+                    lines = [line.strip() for line in open(ak_path) if line.strip()]
+
+                    # iterate over each line in the authorized keys file
+                    for line in lines:
+                        logging.debug(str('processing line: ' + line))
+                        # return immediately when line is prefixed with # (a comment)
+                        if line.startswith('#'):
+                            logging.debug('Skipping line with comment')
+                        else:
+                            try:
+                                public_key = PublicKey(line)
+                                if public_key.comment == '':
+                                    label = '<none>'
+                                else:
+                                    label = public_key.comment
+
+                                logging.debug("* key = %r" % public_key)
+                                logging.debug("  - prefix = %r" % public_key.prefix)
+                                logging.debug("  - algo = %r" % public_key.algo)
+                                logging.debug("  - comment = %r" % public_key.comment)
+                                logging.debug("  - options = %r" % public_key.options)
+                                key_material = base64.b64decode(public_key.blob.decode("utf-8"))
+                                logging.debug("  - key_material = %r" % key_material)
+                                
+                                print('found ', ak_path, ':', label, ':', key_material[0:12] + '...' + key_material[-19:])
+                            except ValueError as e:
+                                logging.debug("* failure = %r" % e)
+
+                            # for interactive mode, ask the user what to do with each detected key
+                            if args.interactive:
+                                if yesno('==> Remove key (y/n) ? '):
+                                    remove_public_key(ak_path, key_material)
+                                    logging.info(Fore.GREEN + "Key '" + label + "' removed.")
+                                else:
+                                    logging.debug(Fore.GREEN + "Key removal of '" + label + "' skipped.")
+                                    if not logging.getLogger().isEnabledFor(logging.DEBUG):
+                                        print()
+
+        logging.info('audit complete.')
         sys.exit()
